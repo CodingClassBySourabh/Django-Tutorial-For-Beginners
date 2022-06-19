305c1dfce9d6891e4a348680c59b1cf87879f618
CodingClassBySourabh <106966192+CodingClassBySourabh@users.noreply.github.com>
2022-06-19 10:27:27 +0530

    Video 9 Completed

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    mainHeading = models.CharField(max_length=200)
+    mainImage = models.FileField(upload_to="Image")
+    qoutes = models.TextField()
+    qoutes_author = models.CharField(max_length=100)
+    content = models.TextField()        
+    date = models.DateField()
